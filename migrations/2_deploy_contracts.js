6f8cb69553459a63de39cdd21a790ed1f81d3f8b
mastrmind-dev <asapthaka@gmail.com>
2022-01-08 22:31:33 +0530

    send tokens to ethSwap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
 };
