f8ee95b3b38984a35e9149f353aae89fbb9eec61
mastrmind-dev <asapthaka@gmail.com>
2022-01-21 03:38:41 +0530

    Complete the backend

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,15 +1,52 @@
 pragma solidity ^0.5.0;
 
-import './Token.sol';
+import "./Token.sol";
 
-contract EthSwap{
+contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
-    
-    constructor (Token _token) public {
+    uint256 public rate = 100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    event TokenSold(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    constructor(Token _token) public {
         token = _token;
     }
 
-    
+    function buyTokens() public payable {
+        uint256 tokenAmount = msg.value * rate;
 
+        require(
+            token.balanceOf(address(this)) >= tokenAmount,
+            "No enough tokens"
+        );
+
+        token.transfer(msg.sender, tokenAmount);
+
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint256 _amount) public {
+        require(token.balanceOf(msg.sender) >= _amount);
+        uint256 etherAmount = _amount / rate;
+
+        require(address(this).balance >= etherAmount, 'no enough ethers');
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        emit TokenSold(msg.sender, address(token), _amount, rate);
+    }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -14,9 +14,9 @@ function tokens(n) {
 contract("EthSwap Smart Contract", (accounts) => {
   var ethSwap, token;
   before(async () => {
-    ethSwap = await EthSwap.new();
     token = await Token.new();
-    await token.transfer(ethSwap.address, tokens('1000000'));
+    ethSwap = await EthSwap.new(token.address);
+    await token.transfer(ethSwap.address, tokens("1000000"));
   });
 
   describe("EthSwap deployment", async () => {
@@ -27,7 +27,7 @@ contract("EthSwap Smart Contract", (accounts) => {
 
     it("contract has tokens", async () => {
       const balance = await token.balanceOf(ethSwap.address);
-      assert.equal(balance.toString(),  tokens('1000000'));
+      assert.equal(balance.toString(), tokens("1000000"));
     });
   });
 
@@ -35,6 +35,60 @@ contract("EthSwap Smart Contract", (accounts) => {
     it("contract has a name", async () => {
       const name = await token.name();
       assert.equal(name, "Sap Token");
+    });
+  });
+
+  describe("buyTokens()", async () => {
+    let result;
+    before(async () => {
+      result = await ethSwap.buyTokens({
+        from: accounts[1],
+        value: tokens("1"),
+      });
+    });
+
+    it("Purchase in ether for sap tokens", async () => {
+      let balance = await token.balanceOf(accounts[1]);
+      assert.equal(balance.toString(), tokens("100"));
+      //to check the token balance of ethSwap after transaction
+      balance = await token.balanceOf(ethSwap.address);
+      assert.equal(balance.toString(), tokens("999900"));
+      //to check the ethereum balance of ethSwap after transaction
+      balance = await web3.eth.getBalance(ethSwap.address);
+      assert.equal(balance.toString(), tokens("1"));
+
+      const event = result.logs[0].args;
+      assert.equal(event.account, accounts[1]);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), tokens("100"));
+      assert.equal(event.rate.toString(), "100");
+    });
+  });
+
+  describe("sell token", async () => {
+    let result;
+    before(async () => {
+      await token.approve(ethSwap.address, tokens("100"), {
+        from: accounts[1],
+      });
+      result = await ethSwap.sellTokens(tokens("100"), { from: accounts[1] });
+    });
+
+    it("token balance of investor", async () => {
+      let balance = await token.balanceOf(accounts[1]);
+      assert.equal(balance.toString(), tokens("0"));
+
+      balance = await token.balanceOf(ethSwap.address);
+      assert.equal(balance.toString(), tokens("1000000"));
+
+      balance = await web3.eth.getBalance(ethSwap.address);
+      assert.equal(balance.toString(), tokens("0"));
+
+      const event = result.logs[0].args;
+      assert.equal(event.account, accounts[1]);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), tokens("100"));
+      assert.equal(event.rate.toString(), "100");
     });
   });
 });
