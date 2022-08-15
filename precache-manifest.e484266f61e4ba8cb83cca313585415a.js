283aaa4b971f15111812186abe9fdbf0dc18795d
mastrmind-dev <asapthaka@gmail.com>
2022-08-15 22:15:39 +0530

    Updates

---
+++ precache-manifest.e484266f61e4ba8cb83cca313585415a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad0e15a7d7f94f160e7",
+    "url": "/crypto-exchange-system/static/js/main.5ad0e15a.chunk.js"
+  },
+  {
+    "revision": "f7fb233be3cb1230dcef",
+    "url": "/crypto-exchange-system/static/css/1.b08252d8.chunk.css"
+  },
+  {
+    "revision": "f7fb233be3cb1230dcef",
+    "url": "/crypto-exchange-system/static/js/1.f7fb233b.chunk.js"
+  },
+  {
+    "revision": "bc86126db035e4ec008a",
+    "url": "/crypto-exchange-system/static/js/runtime~main.bc86126d.js"
+  },
+  {
+    "revision": "579942ccb4684022f725224c28e95175",
+    "url": "/crypto-exchange-system/index.html"
+  }
+];
